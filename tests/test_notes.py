3a0da2c87bd328f1c555a02caa91136ba10ac560
Alex Li <sclion.by@gmail.com>
2026-04-29 13:19:03 +0300

    Improve tests coverage

--- tests/test_notes.py
+++ tests/test_notes.py
@@ -1,11 +1,18 @@
-from app.service import add_note, get_notes
+from app.service import add_note, get_notes, update_note, delete_note, clear
 
-def test_add_note():
-    add_note("Test")
-    assert len(get_notes()) > 0
-
-from app.service import update_note
+def test_add():
+    clear()
+    add_note("Hello")
+    assert len(get_notes()) == 1
 
 def test_update():
-    note = update_note(1, "New")
-    assert note["text"] == "New"
+    clear()
+    add_note("Old")
+    updated = update_note(1, "New")
+    assert updated["text"] == "New"
+
+def test_delete():
+    clear()
+    add_note("Test")
+    delete_note(1)
+    assert len(get_notes()) == 0
