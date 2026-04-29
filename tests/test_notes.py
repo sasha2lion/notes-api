069e2b42106f0ad40f4b612e73b97865e2b52af7
Alex Li <sclion.by@gmail.com>
2026-04-29 12:56:33 +0300

    Add update test
    Add update test

--- tests/test_notes.py
+++ tests/test_notes.py
@@ -3,3 +3,9 @@ from app.service import add_note, get_notes
 def test_add_note():
     add_note("Test")
     assert len(get_notes()) > 0
+
+from app.service import update_note
+
+def test_update():
+    note = update_note(1, "New")
+    assert note["text"] == "New"
